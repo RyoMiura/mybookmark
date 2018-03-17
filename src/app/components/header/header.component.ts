9bf7d292f911042fdc7a6ff5285b2aec1d95bd31
ryo-miura <ryomiura0322@gmail.com>
2018-03-17 18:25:47 +0900

    add new cord of header's search logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TagService } from '@app/service/tag/tag.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  searchText: string;
+
+  constructor(private router: Router, private tagService: TagService) { }
 
   ngOnInit() {
+  }
+
+  goSearch(): void {
+    var this_ = this;
+    this.tagService.findTags()
+      .subscribe(tags => {
+        var tag = tags.find(tag => tag.name === this_.searchText);
+        var id = tag ? tag.id : -1;
+        this_.router.navigate(['/bookmarks/search'], { queryParams: { 'tag': id } });
+      });
   }
 
 }
